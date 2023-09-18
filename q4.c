5d330bf2f5e18bdc440b78c02a7ce94a3c509376
knalen25 <142228584+knalen25@users.noreply.github.com>
2023-09-18 13:41:19 -0300

    q4.c

---
+++ q4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+    char pls;
+    float salario, soma;
+
+    printf("digite o salario:\n");
+    scanf("%f", &salario);
+
+    printf("digite o nivel de experiencia do funcionario a, b ou c:\n");
+    scanf(" %c", &pls);
+
+    switch (pls)
+    {
+    case 'a':
+        soma = salario + salario*0.05;
+
+        printf("o novo salario e de:R$%.2f\n", soma);    
+        break;
+
+    case 'b':
+        soma = salario + salario*0.08;
+        printf("o novo salario e de:R$%.2f\n", soma);
+        break;
+    
+    case 'c':
+        soma = salario + salario*0.07;
+        printf("o novo salario e de:R$%.2f\n", soma);
+        break;
+        
+    default:
+        printf("nivel de experiencia nao reconhecido");
+        break;
+    }
+
+}
