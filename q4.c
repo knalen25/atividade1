b77cda35a21607a318eafbbdadbd6a4809156417
knalen25 <142228584+knalen25@users.noreply.github.com>
2023-09-24 08:24:21 -0300

    Update q4.c

--- q4.c
+++ q4.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
-//trocar float por double
-
 int main(){
 
     char pls;
-    float salario, soma;
+    double salario, soma;
+ 
+    //printf("digite o nivel de experiencia do funcionario a, b ou c:\n");
+    scanf("%c", &pls);
 
-    printf("digite o salario:\n");
-    scanf("%f", &salario);
+     //printf("digite o salario:\n"); 
+    scanf("%lf", &salario);
 
-    printf("digite o nivel de experiencia do funcionario a, b ou c:\n");
-    scanf(" %c", &pls);
+   
+   
 
     switch (pls)
     {
     case 'a':
         soma = salario + salario*0.05;
 
-        printf("o novo salario e de:R$%.2f\n", soma);    
+        printf("R$ %.2lf\n", soma);    
         break;
 
     case 'b':
-        soma = salario + salario*0.08;
-        printf("o novo salario e de:R$%.2f\n", soma);
+        soma = salario + salario*0.07;
+        printf("R$ %.2lf\n", soma);
         break;
     
     case 'c':
-        soma = salario + salario*0.07;
-        printf("o novo salario e de:R$%.2f\n", soma);
+        soma = salario + salario*0.08;
+        printf("R$ %.2lf\n", soma);
         break;
         
     default:
