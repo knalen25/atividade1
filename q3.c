135b757a74b6ed661d092837c2fda410a1fd1dff
knalen25 <142228584+knalen25@users.noreply.github.com>
2023-09-24 08:18:35 -0300

    Update q3.c

--- q3.c
+++ q3.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
-
-
 int main(){
     int senha, login;
   
-    printf("cadastre sua senha:");
+    //printf("cadastre sua senha:");
     scanf("%d", &senha);
+
+    printf("senha cadastrada: %d\n", senha);
     
-    printf("digite a senha para fazer login:");
+    //printf("digite a senha para fazer login:");
     scanf("%d",&login);
     
 
     if (senha == login)
     {
-        printf("senha valida!");
+        printf("senha valida!\n");
     }
+
+  /*while (senha!= login)
+  {
+        printf ("senha invalida\n");
+        printf("digite a senha novamente para fazer o login\n");
+  }*/
   
     for (; senha != login;)
     {
-        printf ("\nsenha invalida!");
-        printf("\ndigite a senha novamente para fazer o login:");
+        printf ("senha invalida!\n");
         scanf("%d", &login);
 
         if (senha == login)
     {
-        printf("senha valida!");
+        printf("senha valida!\n");
     }
 
     }
