ad4f107a0c79b1ac90cfad68bdb2861d30511a0f
Mateus Amorim <142228584+knalen25@users.noreply.github.com>
2023-09-27 22:24:41 -0300

    Update q5.c

--- q5.c
+++ q5.c
@@ -4,10 +4,10 @@
 int main(){
     int a, b;
 
-    //printf("digite o valor de A:\n");
+
     scanf("%d",&a);
 
-    //printf("digite o valor de B:\n");
+
     scanf("%d", &b);
 
     if (a > b && a%b == 0)
@@ -37,11 +37,11 @@ int main(){
 
     else
     {
-        printf("os valores lidos sao iguais");
+        printf("Os valores lidos sao iguais");
     }
     
     
     
-    
+    return 0;
     
 }
