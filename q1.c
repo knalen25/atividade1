924465f0bebeb962cd969e8b265645494fb31cc1
Mateus Amorim <142228584+knalen25@users.noreply.github.com>
2023-09-28 09:29:49 -0300

    Update q1.c

--- q1.c
+++ q1.c
@@ -2,66 +2,27 @@
 
 
 int main(){
-    int ano,r, resto;
+    int ano,r;
 
-    //printf("digite o ano:\n");
     scanf("%d", &ano);
 
     r = ano - 1930;
-    resto = r%4;
 
-    if (ano>=1896 && ano<=1929)
-    {
-            if (ano == 1896)
-            {
-                printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
-            }
-            
 
-            else if (ano%4 == 0){
-                printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
-            }
-            else if(ano%4 != 0)
-            {
-                  printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
-             } 
-      
-    }
-
-    else if (ano%4 == 0 && ano > 1896 && ano<=2022)
+    if (ano>1895 && ano<2023 && ano!=1916 && ano!=1940 && ano!=1944 && ano%4 == 0 )
     {
         printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
     }
-    
-    
-    else if (ano>= 1930 && ano<=2022)
-    {
-          if(r == 0)
-    {
-        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
-    }
 
-    else if (resto == 0)
+    else if ( ano > 1929 && ano < 2023 && ano!=1942 && ano!= 1946 && r%4 == 0 || r == 0 )
     {
        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
-
     }
+
     else
     {
         printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
     }
 
-    }
-
-    else if (ano>2022)
-    {
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
-    }
-    
-    else if(ano<1896){
-        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
-    }
-    
-    
-     
+    return 0;     
 }
